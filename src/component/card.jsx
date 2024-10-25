453053d3858324e8ff58b69003a497f910262d57
bkb <tayyabu207@gmail.com>
2024-10-25 11:00:00 +0500

    format doc

--- src/component/card.jsx
+++ src/component/card.jsx
@@ -1,24 +1,30 @@
 import PropTypes from 'prop-types';
+
 import './style/card.css';
 
-Card_temp.propTypes = {
+Card_temp.propTypes = 
+{
     img: PropTypes.string.isRequired,
+    
     title: PropTypes.string.isRequired,
     text: PropTypes.string.isRequired,
 };
 
 
-function Card_temp  ({text,img,title}) {
 
-return (
-    <div id = "box">
-        <ul>
-           <img src= {img} /> 
-            <h4>{title}</h4>
-            <p>{text}</p>
-        </ul>
-    </div>
-);
+
+
+function Card_temp({ text, img, title }) {
+
+    return (
+        <div id="box">
+            <ul>
+                <img src={img} />
+                <h4>{title}</h4>
+                <p>{text}</p>
+            </ul>
+        </div>
+    );
 
 
 }
